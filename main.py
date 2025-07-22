8800713a363ecc0eb3b6fd06d86207b45527c598
yjw2807 <149188438+yjw2807@users.noreply.github.com>
2025-07-22 08:57:22 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#Everyone write a comment to make sure can access edit this thing
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.manifold import TSNE
+from sklearn.neighbors import KNeighborsClassifier
+from ipywidgets import interact, IntSlider, Button, HBox, VBox
